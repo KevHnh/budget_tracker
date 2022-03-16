2a142d82c6ff8b0d316ee178fd07a20bb8d2cd0b
KevHnh <kevinhinh9@gmail.com>
2022-03-16 19:13:22 -0400

    new

---
+++ budget_tracker/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:budget_tracker/form_panel.dart';
+import 'package:budget_tracker/all_panels.dart';
+import 'login.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Budget Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const AllPanels();
+          }
+          return const CircularProgressIndicator();
+        },
+      )
+    );
+  }
+}
